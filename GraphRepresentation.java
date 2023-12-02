f2b96b659f08a2b1a1efcbddc9fd4237ea269d85
Rajesh kumar jena <129972543+Rajesh-1234567@users.noreply.github.com>
2023-12-02 22:00:38 +0530

    Create GraphRepresentation.java

---
+++ GraphRepresentation.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class represent {
+    static class Edge{
+        int src,dest,wt;
+        Edge(int s,int d,int w){
+            this.src=s;
+            this.dest=d;
+            this.wt=w;
+        }
+    }
+    
+    public static void main(String []args){
+        int v=5;
+        ArrayList<Edge> graph[]= new ArrayList[v];
+        for(int i=0;i<v;i++){
+            graph[i]= new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,1,5));// 0 index
+        graph[1].add(new Edge(1,0,5));// 1st index
+        graph[1].add(new Edge(1,2,1));
+        graph[1].add(new Edge(1,3,3));
+        graph[2].add(new Edge(2,1,1));// 2nd index
+        graph[2].add(new Edge(2,3,1));
+        graph[2].add(new Edge(2,4,4));
+        graph[3].add(new Edge(3,1,3));// 3rd index
+        graph[3].add(new Edge(3,2,1));
+        graph[4].add(new Edge(4,2,2));
+        //2's neighbour
+        for(int i=0;i<graph[2].size();i++){
+            Edge e= graph[2].get(i);
+            System.out.println(e.dest);
+        }
+
+
+    }
+}
