50fa1df22280a6c2e03577b63090d31a718ddc7c
Rajesh kumar jena <129972543+Rajesh-1234567@users.noreply.github.com>
2023-12-03 21:31:36 +0530

    Create BFS.java

---
+++ BFS.java
@@ -0,0 +1,56 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Graph {
+    private int vertices;
+    private LinkedList<Integer>[] adjacencyList;
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        this.adjacencyList = new LinkedList[vertices];
+
+        for (int i = 0; i < vertices; i++) {
+            this.adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int source, int destination) {
+        this.adjacencyList[source].add(destination);
+        // For undirected graph, add the reverse edge as well
+        this.adjacencyList[destination].add(source);
+    }
+
+    public void bfs(int startVertex) {
+        boolean[] visited = new boolean[vertices];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[startVertex] = true;
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            int currentVertex = queue.poll();
+            System.out.print(currentVertex + " ");
+
+            for (int neighbor : adjacencyList[currentVertex]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        Graph graph = new Graph(6);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 5);
+
+        System.out.println("BFS starting from vertex 0:");
+        graph.bfs(0);
+    }
+}
