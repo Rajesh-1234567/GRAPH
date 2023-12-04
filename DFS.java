c3952ef31b6fad71047dea1efdb3ffbbe2283314
Rajesh kumar jena <129972543+Rajesh-1234567@users.noreply.github.com>
2023-12-04 18:31:13 +0530

    Create DFS.java

---
+++ DFS.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Graph {
+    private int vertices;
+    private LinkedList<Integer>[] adjacencyList;
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        adjacencyList = new LinkedList[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adjacencyList[v].add(w);
+    }
+
+    public void DFS(int startVertex) {
+        boolean[] visited = new boolean[vertices];
+        DFSUtil(startVertex, visited);
+    }
+
+    private void DFSUtil(int vertex, boolean[] visited) {
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+
+        for (int neighbor : adjacencyList[vertex]) {
+            if (!visited[neighbor]) {
+                DFSUtil(neighbor, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(4);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 0);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 3);
+
+        System.out.println("Depth-First Traversal (starting from vertex 2):");
+        graph.DFS(2);
+    }
+}
